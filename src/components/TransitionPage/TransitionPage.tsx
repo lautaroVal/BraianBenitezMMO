625734e032534d50b1b824f6a095242eddeccafc
lautaroVal <valenzuelalautaro@yahoo.com>
2025-07-19 03:11:54 -0300

    Creando componente Transition

--- src/components/TransitionPage/TransitionPage.tsx
+++ src/components/TransitionPage/TransitionPage.tsx
@@ -1,8 +1,43 @@
+import { AnimatePresence, easeInOut, motion } from "framer-motion"
 
 export function TransitionPage() {
     return (
-        <>
-            TransitionPage
-            </>
+        <AnimatePresence mode="wait">
+            <motion.div 
+            className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-secondary"
+            variants={transitionVariantPage}
+            initial="initial" 
+            animate="animate"
+            exit="exit"
+            transition={{delay: 0.2, duration: 0.6, ease:easeInOut}}
+            >
+
+            </motion.div>
+            <motion.div 
+            className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-secondary/70 opacity-50"
+            variants={transitionVariantPage}
+            initial="initial" 
+            animate="animate"
+            exit="exit"
+            transition={{delay: 0.4, duration: 0.6, ease:easeInOut}}
+            >
+
+            </motion.div>
+            </AnimatePresence>
     )
+}
+
+const transitionVariantPage = {
+    initial: {
+        x: "100%",
+        width: "100%"
+    },
+    animate: {
+        x: "0%",
+        width: "0%"
+    },
+    exit: {
+        x: ["0%","100%"],
+        width: ["0%","100%"],
+    }
 }
